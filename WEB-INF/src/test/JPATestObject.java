0a8840e20ec9fce6571169ab9cfd337ecb8d149c
tobias <tobiasmichaelfeller@gmail.com>
2014-04-21 00:32:38 +0200

    ~ Testing proper github config.

--- WEB-INF/src/test/JPATestObject.java
+++ WEB-INF/src/test/JPATestObject.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+
 @Entity
 public class JPATestObject implements Serializable {
 	private static final long serialVersionUID = 1L;
