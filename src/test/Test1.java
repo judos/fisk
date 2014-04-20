a9157401b0ae41cbe09e4c69ca968cb76f09031d
judos <judos@gmx.ch>
2014-04-20 12:39:39 +0200

    test

---
+++ src/test/Test1.java
@@ -0,0 +1,17 @@
+package test;
+
+/**
+ * @since 20.04.2014
+ * @author Julian Schelker
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
