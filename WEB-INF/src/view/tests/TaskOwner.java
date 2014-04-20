4f7181f3f9fc242d1deb5c14da165bf0d528781e
tobias <tobias.feller@stud.hslu.ch>
2014-04-20 22:01:04 +0200

    ADD: JavaDoc in TaskOwner class.

--- WEB-INF/src/view/tests/TaskOwner.java
+++ WEB-INF/src/view/tests/TaskOwner.java
@@ -1,7 +1,21 @@
 package view.tests;
 
+/**
+ * This interface provides a method for owners of Task to be informed on when their Task is done.
+ * 
+ * @see Task
+ * @see TaskHandler
+ * @author tobias
+ *
+ */
 public interface TaskOwner {
 	
+	/**
+	 * Accepts Task objects which are done according to the server time in seconds. 
+	 * 
+	 * @see ServerClock
+	 * @param task Task object that is done.
+	 */
 	public void onTaskReady(Task task);
 
 }
